c9a1d44ea9ba9b5f6d896bca0a9a58ac7f28ba14
Jesse Manninen <jesse.manninen@gmail.com>
2023-09-01 19:16:15 +0300

    env to ignorre

--- src/features/store/storeAPI.ts
+++ src/features/store/storeAPI.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api: string | undefined = process.env.REACT_APP_STORE_API_URL;
 
 if (!api) {
-    throw new Error('REACT_APP_API_URL is not defined!');
+    throw new Error('REACT_APP_STORE_API_URL is not defined!');
 }
 
 export const fetchProducts = createAsyncThunk(
